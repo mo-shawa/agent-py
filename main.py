83b88dda3f9e94e0805a2a87bd7e807125184184
moshawa <moshawa0@gmail.com>
2025-11-08 13:51:10 -0500

    feat: support verbose flag

--- main.py
+++ main.py
@@ -3,25 +3,39 @@ from dotenv import load_dotenv
 from google import genai
 import sys
 from google.genai import types
+import argparse
 
 load_dotenv()
+
+parser = argparse.ArgumentParser()
+parser.add_argument("prompt", help="User prompt string")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+args = parser.parse_args()
 
 api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
 
-prompt = sys.argv[1]
+prompt = args.prompt
+
+if not prompt:
+    sys.exit(1)
 
 messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
 
-if not prompt:
-  sys.exit(1)
+response = client.models.generate_content(
+    model="gemini-2.0-flash-001", contents=messages
+)
 
-response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+if args.verbose:
+    print(f"User prompt: {prompt}")
 
-print(response.text)
+print(f"\n{response.text}")
 
-print(f"""
+if args.verbose:
+    print(
+        f"""
 Prompt tokens: {response.usage_metadata.prompt_token_count}
 Response tokens: {response.usage_metadata.candidates_token_count}
-""")
+"""
+    )
