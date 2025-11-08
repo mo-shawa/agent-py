4cc47dda4eaa0f1c9675eda586340b2c59e1d314
moshawa <moshawa0@gmail.com>
2025-11-08 12:32:03 -0500

    feat: using messages array instead of prompt directly

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from google import genai
 import sys
+from google.genai import types
 
 load_dotenv()
 
@@ -11,10 +12,12 @@ client = genai.Client(api_key=api_key)
 
 prompt = sys.argv[1]
 
+messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
+
 if not prompt:
   sys.exit(1)
 
-response = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
+response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
 
 print(response.text)
 
